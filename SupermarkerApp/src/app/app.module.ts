e8b7d6c1785cede990b5c0483f10d0eb94e1bcc0
Will-Morales-Dev <willmoralesdev@gmail.com>
2022-12-22 11:30:06 -0600

    GENERACIÓN DE COMPONENTE -> lista-clientes

--- SupermarkerApp/src/app/app.module.ts
+++ SupermarkerApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ListaClientesComponent } from './lista-clientes/lista-clientes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ListaClientesComponent
   ],
   imports: [
     BrowserModule,
